98b519cebd65cfbbedfb6e73781ea23a8326a7e2
romerorodriguez <a3522110182@alumno.uttehuacan.edu.mx>
2025-10-05 22:57:18 -0600

    feat: cambio de pantallas

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
-import './App.css';
-import { useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Task from './Task';
+import Home from './Home';
 
-function App() {
-    const navigate = useNavigate();
-    const goToTask = () => {
-        navigate('/task');
-    }
-  
+function MainApp() {
   return (
-    <div className="app-shell">
-      <header>
-        <h1>TaskFlow - JRR</h1>
-      </header>
-      <main>
-        <p>Tu organizador de tareas offline</p>
-      </main>
-      <footer>
-        <small>© 2025 TaskFlow</small>
-      </footer>
-      <button onClick={goToTask}>
-        Comenzar
-      </button>
-      <button>
-        Descargar App
-      </button>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/task" element={<Task />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App;
+export default MainApp;
